54c5f05c5d9209bdf81a355cb538d887253769a7
Glitch (glitch-blank-website) <none>
2022-04-15 18:10:31 +0000

    🐊🎪 Checkpoint ./script.js:28956677/3892

--- script.js
+++ script.js
@@ -3,18 +3,26 @@
 
 console.log("Hello, world!");
 
-
 // Set the date we're counting down to
-var timeleft = 99;
 
-var downloadTimer = setInterval(function(){
-    if(timeleft == 0){
-    // clearInterval(downloadTimer);
-    document.getElementById("number").innerHTML = "0";
-      timeleft=100;
-  } else {
-    document.getElementById("number").innerHTML = timeleft + " seconds remaining";
-  }
-  timeleft -= 1;
+function reset() {
+  var timeleft = 3;
+  var downloadTimer = setInterval(function () {
+    if (timeleft == 0) {
+      clearInterval(downloadTimer);
+      document.getElementById("number").innerHTML = "0";
+      document.addEventListener("click", function () {
+        timeleft = 3;
+        reset();
+        // console.log("clicked");
+      });
+      // timeleft=100;
+    } else {
+      document.getElementById("number").innerHTML =
+        timeleft + " seconds remaining";
+    }
+    timeleft -= 1;
+  }, 1000);
 }
-                           , 1000);
+
+reset();
