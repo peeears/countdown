0ce5ed987aa6b0dc923fdbefe396e3e3c767b5e8
Glitch (glitch-blank-website) <none>
2022-04-15 17:22:30 +0000

    🍐🌼 Checkpoint ./script.js:28956677/524

--- script.js
+++ script.js
@@ -2,3 +2,17 @@
     to your site with Javascript */
 
 console.log("Hello, world!");
+
+
+// Set the date we're counting down to
+var timeleft = 99;
+
+var downloadTimer = setInterval(function(){
+  if(timeleft <= 0){
+    clearInterval(downloadTimer);
+    document.getElementById("number").innerHTML = "Finished";
+  } else {
+    document.getElementById("number").innerHTML = timeleft + " seconds remaining";
+  }
+  timeleft -= 1;
+}, 1000);
