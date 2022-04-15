4c7ea10bf47d7ff8ca378fd1429b9de07d8b9218
Glitch (glitch-blank-website) <none>
2022-04-15 19:19:36 +0000

    🍠👓 Checkpoint ./script.js:28956677/5420 ./index.html:28956677/180

--- script.js
+++ script.js
@@ -25,15 +25,69 @@ function reset() {
 
 reset();
 
-
 var countdown = document.getElementById("countdown");
+var options = document.querySelectorAll(".option");
+var image=document.querySelector(".img");
 
-var options=document.getElementsByTagName("option") ;
-options.forEach(function(option) 
+countdown.addEventListener("change", function () {
+  var countdownval = this.value;
+  // console.log(countdownval);
+  switch (countdownval) {
+    case 0:
+      // day = "Sunday";
+      break;
+    case 1:
+      // day = "Monday";
+      break;
+    case 2:
+      // day = "Tuesday";
+      break;
+    case 3:
+      // day = "Wednesday";
+      break;
+    case 4:
+      // day = "Thursday";
+      break;
+    case 5:
+      // day = "Friday";
+      break;
+    case 6:
+      // day = "Saturday";
+      break;
+      case 7:
+      // day = "Saturday";
+      break;
+      case 8:
+      // day = "Saturday";
+      break;
+      case 9:
+      // day = "Saturday";
+      break;
+      case 10:
+      // day = "Saturday";
+      break;
+      case 11:
+      console.log("11");
+      image.src="https://cdn.glitch.global/693438a2-770f-48f5-8219-74763b09679b/ae-count-03.png?v=1649993902587"
+      break;
+  }
+});
 
-  option.addEventListener('click', function() {
-    var countdownval = this.value;
-  console.log(countdownval)
-    // document.getElementById('comment-form-' + postId).style.display = 'block'
-  })
-}))
+// options.forEach(function(option) {
+
+//   option.addEventListener('change', function() {
+//     // var postId = this.getAttribute('post-id')
+//   console.log("click")
+//     // document.getElementById('comment-form-' + postId).style.display = 'block'
+//   })
+// })
+
+// console.log(options);
+// options.forEach(function(option) {
+//   option.addEventListener('click', function() {
+//     console.log("click")
+//     var countdownval = this.value;
+//   console.log(countdownval)
+//     // document.getElementById('comment-form-' + postId).style.display = 'block'
+//   })
+// })
