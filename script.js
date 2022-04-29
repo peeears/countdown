1e1d90a92031ac2341c5ea27cfdaa99e2fb80990
Glitch (glitch-blank-website) <none>
2022-04-29 03:47:58 +0000

    🌶🚘 Checkpoint ./script.js:28956677/406 ./index.html:28956677/21 ./style.css:28956677/8400

--- script.js
+++ script.js
@@ -3,11 +3,13 @@
 
 // console.log("Hello, world!");
 
-let srclist=[]
+let srclist=["1","2","3","4","https://cdn.glitch.global/b8f7fe36-fd1a-40e9-8a86-3605c11486bc/path.mp4?v=1651201989036","https://cdn.glitch.global/b8f7fe36-fd1a-40e9-8a86-3605c11486bc/walk.mp4?v=1651201989092","7","8","9","10"]
 
 $( ".num" ).click(function(e) {
   e.preventDefault();
-  // console.log($(this))
-  $(".vd").attr('src', newmp4);
+  let index=$(this).children("a").attr("id");
+   // console.log(srclist[index]);
+  $(".vd").attr('src', srclist[index]);
+  $(".backgroundVD").show();
 });
 
