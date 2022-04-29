db6f73a4736da32c5fba224b9b58f756508c7e73
Glitch (glitch-blank-website) <none>
2022-04-29 12:12:31 -0400

    push

--- script.js
+++ script.js
@@ -3,36 +3,53 @@
 
 // console.log("Hello, world!");
 
-let srclist=["1","https://cdn.glitch.global/b8f7fe36-fd1a-40e9-8a86-3605c11486bc/cooker.mp4?v=1651206240397","3","4","https://cdn.glitch.global/b8f7fe36-fd1a-40e9-8a86-3605c11486bc/path.mp4?v=1651201989036","https://cdn.glitch.global/b8f7fe36-fd1a-40e9-8a86-3605c11486bc/walk.mp4?v=1651201989092","7","8","9","10"]
+let srclist = [
+  "countdownVD/newyear.mp4",
+  "countdownVD/cooker_3.mp4",
+  "countdownVD/oldfilm.mp4",
+  "countdownVD/youtube.mp4",
+  "countdownVD/path.mp4",
+  "countdownVD/walk_2.mp4",
+  "countdownVD/space.mp4",
+  "8",
+  "9",
+  "countdownVD/bomb.mp4",
+];
 let randomX;
 let randomY;
 
-
-$( ".num" ).click(function(e) {
-  randomX=Math.floor((Math.random() * $(window).width()) + 1);
-  randomY=Math.floor((Math.random() * $(window).height()) + 1);
+$(".num").click(function (e) {
+  randomX = Math.floor(Math.random() * $(window).width() + 1);
+  randomY = Math.floor(Math.random() * $(window).height() + 1);
   e.preventDefault();
-  let index=$(this).children("a").attr("id");
-   // console.log(srclist[index]);
-  $(".esc").css({ top: randomY+"px" ,left:randomX+"px"});
-  console.log(randomX,randomY);
-  $(".esc").fadeIn(2000);
-  $(".vd").attr('src', srclist[index]);
+  let index = $(this).children("a").attr("id");
+  // console.log(srclist[index]);
+  $(".esc").css({ top: randomY + "px", left: randomX + "px" });
+  console.log(randomX, randomY);
+  // $(".esc").fadeIn(2000);
+  $(".vd").attr("src", srclist[index]);
   $(".backgroundVD").fadeIn();
-  $('video').on('ended',function(){
+  $("video").on("ended", function () {
+    $(".backgroundVD").fadeOut();
+    // $(".esc").hide();
+  });
+  $(".esc").click(function (event) {
+    event.preventDefault();
+    $(".backgroundVD").fadeOut();
+    // $(".esc").hide();
+  });
+  $(document).keyup(function (e) {
+    if (e.key === "Escape") {
+      // escape key maps to keycode `27`
+      e.preventDefault();
       $(".backgroundVD").fadeOut();
-     $(".esc").hide();
-    });
-  $(".esc").click(function(event){
-     event.preventDefault();
-    $(".backgroundVD").fadeOut();
-     $(".esc").hide();
-  }) 
-  $(document).keyup(function(e) {
-     if (e.key === "Escape") { // escape key maps to keycode `27`
-       event.preventDefault();
-    $(".backgroundVD").fadeOut();
-     $(".esc").hide();
+      // $(".esc").hide();
     }
+  });
 });
+
+$(document).click(function () {
+  // e.preventDefault();
+  $(".audio")[0].play();
+  console.log("click");
 });
